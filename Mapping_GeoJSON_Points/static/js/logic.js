4b0bab9fd544582f6c6ce425b58ceaea6c7294c0
unknown <nasara.ryan@gmail.com>
2021-03-26 16:11:23 -0400

    adding majorAirports dataset and added initial GeoJson marker

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -3,30 +3,51 @@
 console.log("working");
 
 //creates the map object with a center and zoom level
-let map = L.map('mapid').setView([36.1733, -120.1794], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-//LAX to SFO coordinates
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-]; 
-
-// creating polyline using line coordinates and makes line red
-L.polyline(line, {
-    color: "blue",
-    weight: 4,
-    dashArray: "5, 7",
-    opacity: 0.5
-    
+//Add GeoJSON Data
+let sanFranAirport = 
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+/*
+//add GeoJson data layer to the map using the pointToLayer function
+L.geoJson(sanFranAirport,{
+    //we turn each feature into a marker on the map
+    pointToLayer: function(feature, latlng){
+        console.log(feature);
+        return L.marker(latlng)
+        .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>")
+    }
 }).addTo(map);
+*/
 
+
+L.geoJson(sanFranAirport, {
+    onEachFeature: function(feature, layer) {
+        console.log(layer)
+        layer.bindPopup("<h2>" +"Airport Code:  " +  feature.properties.faa + "</h2> <hr> <h3>" + "Aiport Name: " +feature.properties.name + "</h3>");
+     }
+}).addTo(map);
 
 //This is the Static Tiles API Format in the leaflet tileLayer() method
 //To use the mapbox Styles API built into the mapbox
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-guidance-night-v4/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
