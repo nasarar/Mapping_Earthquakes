85fd0468b64657e591b3354f413f26486dad2db1
unknown <nasara.ryan@gmail.com>
2021-03-25 22:21:09 -0400

    added popup binds and circle marker for practice dataset

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -5,41 +5,15 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 
- //adding multiple markers on the map
- let cities = [{
-    location: [40.7128, -74.0059],
-    city: "New York City",
-    state: "NY",
-    population: 8398748
-  },
-  {
-    location: [41.8781, -87.6298],
-    city: "Chicago",
-    state: "IL",
-    population: 2705994
-  },
-  {
-    location: [29.7604, -95.3698],
-    city: "Houston",
-    state: "TX",
-    population: 2325502
-  },
-  {
-    location: [34.0522, -118.2437],
-    city: "Los Angeles",
-    state: "CA",
-    population: 3990456
-  },
-  {
-    location: [33.4484, -112.0740],
-    city: "Phoenix",
-    state: "AZ",
-    population: 1660272
- }];
+//adding multiple markers on the map
+//get data from cities.js
+ let cityData = cities;
  //looping through the cities array
- cities.forEach(function(city) {
+ cityData.forEach(function(city) {
     console.log(city)
-    L.marker(city.location).addTo(map);
+    L.circleMarker(city.location,{ radius: city.population/100000})
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
    });
 
 
