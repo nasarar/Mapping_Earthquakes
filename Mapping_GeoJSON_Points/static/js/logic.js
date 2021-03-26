1c8b8aabbf6b6e61a41d509f5283b9363dbf08da
unknown <nasara.ryan@gmail.com>
2021-03-26 17:30:33 -0400

    connected airport data to script

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -3,8 +3,11 @@
 console.log("working");
 
 //creates the map object with a center and zoom level
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+let map = L.map('mapid').setView([30, 30], 20);
 
+
+
+/*
 //Add GeoJSON Data
 let sanFranAirport = 
 {"type":"FeatureCollection","features":[{
@@ -24,7 +27,7 @@ let sanFranAirport =
             "type":"Point",
             "coordinates":[-122.375,37.61899948120117]}}
 ]};
-/*
+
 //add GeoJson data layer to the map using the pointToLayer function
 L.geoJson(sanFranAirport,{
     //we turn each feature into a marker on the map
@@ -34,15 +37,18 @@ L.geoJson(sanFranAirport,{
         .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>")
     }
 }).addTo(map);
-*/
 
-
+//
 L.geoJson(sanFranAirport, {
     onEachFeature: function(feature, layer) {
         console.log(layer)
         layer.bindPopup("<h2>" +"Airport Code:  " +  feature.properties.faa + "</h2> <hr> <h3>" + "Aiport Name: " +feature.properties.name + "</h3>");
      }
 }).addTo(map);
+
+*/
+
+
 
 //This is the Static Tiles API Format in the leaflet tileLayer() method
 //To use the mapbox Styles API built into the mapbox
@@ -54,4 +60,15 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 //adding 'graymap' tile layer to the map
 streets.addTo(map);
+
+//Add GeoJSON Data
+let airportData = "https://raw.githubusercontent.com/nasarar/Mapping_Earthquakes/main/Mapping_GeoJSON_Points/majorAirports.json";
+
+d3.json(airportData).then(function(data){
+    console.log(data);
+
+    //creates a GeoJSON layer with retrieved data
+    L.geoJson(data).addTo(map);
+});
+
 
